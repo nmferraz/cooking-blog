2fe28611e8d4b0df08ac0d7d10945c49431ff15b
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-04-12 18:18:46 +0100

    setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const fileUpload = require('express-fileupload');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+require('dotenv').config();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(expressLayouts);
+
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+const routes = require('./server/routes/recipeRoutes.js');
+app.use('/', routes);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));

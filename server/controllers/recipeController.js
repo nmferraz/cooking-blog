c865467a8e4988a553dada7a4246ec11ad0d109d
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-04-12 22:09:01 +0100

    working with categories
    begin

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,7 +1,18 @@
+require('../models/database');
+const Category = require('../models/Category');
+
 /**
  * GET /
  * Homepage 
 */
 exports.homepage = async (req, res) => {
-  res.render('index', { title: 'Homepage | Cooking Blog' });
+  try {
+    const limitNumber = 5;
+    const categories = await Category.find({}).limit(limitNumber);
+
+    res.render('index', { title: 'Cooking Blog - Home', categories });
+  } catch (error) {
+    res.satus(500).send({ message: error.message || "Error Occured" });
+  }
 }
+

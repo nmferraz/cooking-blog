4e2393a03a2de4c39690b4ab9830300a8afc72fb
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-04-13 11:24:53 +0100

    show latest recipes

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -6,14 +6,20 @@ const Recipe = require('../models/Recipe');
  * GET /
  * Homepage 
 */
-exports.homepage = async (req, res) => {
+exports.homepage = async(req, res) => {
   try {
     const limitNumber = 5;
     const categories = await Category.find({}).limit(limitNumber);
+    const latest = await Recipe.find({}).sort({_id: -1}).limit(limitNumber);
+    const thai = await Recipe.find({ 'category': 'Thai' }).limit(limitNumber);
+    const american = await Recipe.find({ 'category': 'American' }).limit(limitNumber);
+    const chinese = await Recipe.find({ 'category': 'Chinese' }).limit(limitNumber);
 
-    res.render('index', { title: 'Cooking Blog - Home', categories });
+    const food = { latest, thai, american, chinese };
+
+    res.render('index', { title: 'Cooking Blog - Home', categories, food } );
   } catch (error) {
-    res.satus(500).send({ message: error.message || "Error Occured" });
+    res.satus(500).send({message: error.message || "Error Occured" });
   }
 }
 
